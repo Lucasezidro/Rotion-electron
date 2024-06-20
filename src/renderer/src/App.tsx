b840bb4da4042c545da677d5804f44b24928996e
lucasezidro <lucasezidro7@gmail.com>
2024-06-20 15:04:10 -0300

    create document page

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,19 +1,9 @@
-import { Blank } from '../pages/blank'
+import { Blank } from './pages/blank'
 import { Routes } from './Routes'
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 import './styles/global.css'
 
 export function App() {
-  return (
-    <div className='h-screen w-screen text-rotion-100 flex'>
-      <Sidebar />
-
-      <div className='flex-1 flex flex-col max-h-screen'>
-        <Header />
-
-        <Routes />
-      </div>
-    </div>
-  )
+  return <Routes />
 }
